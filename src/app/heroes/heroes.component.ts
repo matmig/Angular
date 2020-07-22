b9d0628099d88ded51e825c3528e13cc2200ef30
Mateusz Migdał <mateusz.migdal@axbit.com>
2020-07-22 08:46:54 +0200

    feat: 2. Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = { id: 0, name: 'Windstorm' };
+  selectedHero: Hero;
+  heroes: Hero[] = HEROES;
   constructor() {}
 
   ngOnInit(): void {}
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
